8e3bbf79643ffcface4b5b29000ff4a0f90a86e2
doanHa <doanmha96@gmail.com>
2019-02-06 21:49:28 -0600

    Update
    Due to some conflicts, FindContiguousSum.java has to be remade

--- InterestingExample/src/FindContiguousSum.java
+++ InterestingExample/src/FindContiguousSum.java
@@ -1,27 +1,38 @@
+/*
+Problem: find the contiguous sum given a positive number
+
+Requirement: - An array of positive number
+             - The result should comprised of numbers next to each other in the array
+ */
+
+
 public class FindContiguousSum {
-    /* Returns true if the there is a subarray of arr[] with sum equal to
+
+    /* Returns true if the there is a sub-array of arr[] with sum equal to
        'sum' otherwise returns false.  Also, prints the result */
-    static int subArraySum(int arr[], int n, int sum)
+    static void contiguousSum(int arr[], int n, int sum)
     {
       int curr_sum = arr[0], start = 0, i;
 
       // Pick a starting point
       for (i = 1; i <= n; i++)
       {
-        // If curr_sum exceeds the sum, then remove the starting elements
+        // If curr_sum exceeds the sum, then remove the starting elements, and move to the next one
         while (curr_sum > sum && start < i-1)
         {
           curr_sum = curr_sum - arr[start];
           start++;
         }
 
-        // If curr_sum becomes equal to sum, then return true
+        // If curr_sum becomes equal to sum, then print out the result
         if (curr_sum == sum)
         {
-          int p = i-1;
-          System.out.println("Sum found between indexes " + start
-            + " and " + p);
-          return 1;
+          System.out.println("The result is: ");
+          for(int j = start; j < i; j++) {
+            System.out.print(arr[j] + "\t");
+          }
+          System.out.println();
+          //return; //uncomment the return to find only 1 possible solution
         }
 
         // Add this element to curr_sum
@@ -31,15 +42,14 @@ public class FindContiguousSum {
       }
 
       System.out.println("No sub-array found");
-      return 0;
+      return;
     }
 
     public static void main(String[] args)
     {
       int arr[] = {15, 2, 4, 8, 9, 5, 10, 23};
-      int n = arr.length;
       int sum = 38;
-      subArraySum(arr, n, sum);
+      contiguousSum(arr, arr.length, sum);
     }
-  }
 }
+
